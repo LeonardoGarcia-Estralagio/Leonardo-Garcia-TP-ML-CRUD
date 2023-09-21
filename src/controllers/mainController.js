ce3a93337ee488a712a72eca71bd3579ea59ccd7
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-09-21 19:59:16 -0300

    modified: search

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,54 +1,59 @@
-const db =require('../database/models')
+const db = require("../database/models");
 const { Op } = require("sequelize");
 
-const { readJSON, writeJSON } = require("../data");
-const products = readJSON('productsDataBase.json')
-
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
-	index: (req, res) => {
+  index: (req, res) => {
+    const visited = db.Product.findAll({
+      where: {
+        categoryId: 1,
+      },
+    });
 
-		const visited = db.Product.findAll({
-			where : {
-				categoryId : 1
-			}
-		})
+    const sale = db.Product.findAll({
+      where: {
+        categoryId: 2,
+      },
+    });
 
-		const sale = db.Product.findAll({
-			where : {
-				categoryId : 2
-			}
-		})
-
-		Promise.all([visited,sale])
-			.then(([visited,sale]) => {
-				return res.render('index', {
-					visited,
-					sale,
-					toThousand
-				})
-			})
-			.catch(error => console.log(error))
-	},
-	search: (req, res) => {
-		const keywords = req.query.keywords;
-		db.Product.findAll({
-			where: {
-				name: {
-					[Op.like]: '%' + keywords + '%'
-				}
-			}
-		})
-		.then(results => {
-			return res.render('results', {
-				results,
-				keywords,
-				toThousand
-			})
-		})
-		.catch(error => console.log(error));
-	},
+    Promise.all([visited, sale])
+      .then(([visited, sale]) => {
+        return res.render("index", {
+          visited,
+          sale,
+          toThousand,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  search: (req, res) => {
+    const keywords = req.query.keywords;
+    db.Product.findAll({
+      where: {
+        [Op.or]: [
+          {
+            name: {
+              [Op.substring]: keywords,
+            },
+          },
+          {
+            description: {
+              [Op.substring]: keywords,
+            },
+          },
+        ],
+      },
+    })
+      .then((results) => {
+        return res.render("results", {
+          results,
+          keywords,
+          toThousand,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
 };
 
 module.exports = controller;
