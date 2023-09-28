9c8550417a2f07823ddfe97ecc270a3b440ba796
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-09-28 13:06:06 -0300

    fix: config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 module.exports = {
   development: {
-    username: process.env.BD_USERNAME_DEV,
+    username: process.env.DB_USERNAME_DEV,
     password: process.env.DB_PASSWORD_DEV,
     database: process.env.DB_DATABASE_DEV,
     host: process.env.DB_HOST_DEV,
